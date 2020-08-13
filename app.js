422abab758a92c2d1b77702c5807965c676e9f38
hi-ashleyj <ajor023@gmail.com>
2020-08-13 15:51:22 +1200

    Clean up the old app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ let path = require("path");
 let url = require("url");
 let http = require("http");
 let child_process = require("child_process");
+const { parse } = require("path");
 
 let networkingPort = 80;
-let servers = {};
+
+let Manager = {};
+Manager.servers = {};
 
 /**
  * servers: {
@@ -19,7 +22,7 @@ let servers = {};
  * }
  */
 
-let runningServers = {};
+Manager.runningServers = {};
 
 let documentsFolder = path.resolve(os.homedir(), "rpi-ws-manager");
 
@@ -33,24 +36,24 @@ if (!fsSync.existsSync(documentsFolder)) { // Create folders and default users
     }
 } else {
     fs.readFile(path.resolve(documentsFolder, "servers.json"), {encoding: "utf8"}).then((data) => {
-        servers = JSON.parse(data);
+        Manager.servers = JSON.parse(data);
     });
 }
 
-let save = async function() {
+Manager.save = async function() {
     try {
-        await fs.writeFile(path.resolve(documentsFolder, "servers.json"), JSON.stringify(servers));
+        await fs.writeFile(path.resolve(documentsFolder, "servers.json"), JSON.stringify(Manager.servers));
     } catch (err) {
         
     }
     
 };
 
-let spawnServer = function(id) {
+Manager.spawnServer = function(id) {
     
 };
 
-let newServer = async function(payload) {
+Manager.newServer = async function(payload) {
     // payload is a server payload lol
 
     if (payload && payload.id && payload.alias && payload.port) {
@@ -62,9 +65,9 @@ let newServer = async function(payload) {
             runfile: (payload.runfile) ? payload.runfile : "app.js"
         }
 
-        servers[id] = config;
+        Manager.servers[id] = config;
 
-        save();
+        Manager.save();
         await fs.mkdir(path.resolve(documentsFolder, id));
 
         return config;
@@ -73,113 +76,158 @@ let newServer = async function(payload) {
     }
 };
 
-let listFiles = function(id, path) {
+Manager.listFiles = function(id, path) {
 
 }
 
-let getServer = function(id) {
+Manager.getServer = function(id) {
     return (id) ? servers[id] : null;
 };
 
-let listServers = function() {
+Manager.listServers = function() {
     return servers;
 };
 
-let handleAllRequests = function(req, res) {
+// --- HTTP STUFF --- //
+
+let Requests = {};
+
+Requests.path = function(req, res) {
+    // This handles sending the pages required.
     let parseIt = url.parse(req.url, true);
     let pathed = path.parse(req.url);
-
     let needsIndex = (pathed.base) ? "" : "index.html";
+    let pathpath = __dirname + "/remote" + parseIt.pathname + needsIndex;
 
+    if (fsSync.existsSync(pathpath)) {
+        if (pathed.ext == ".html") {
+            res.setHeader("Content-Type", "text/html");
+        } else if (pathed.ext == ".js") {
+            res.setHeader("Content-Type", "application/js");
+        } else if (pathed.ext == ".css") {
+            res.setHeader("Content-Type", "text/css");
+        } else if (pathed.ext == ".png") {
+            res.setHeader("Content-Type", "image/png");
+        } else if (pathed.ext == ".svg") {
+            res.setHeader("Content-Type", "image/svg+xml");
+        } else if (pathed.ext == ".ttf") {
+            res.setHeader("Content-Type", "application/font");
+        }
+
+        let stream = fsSync.createReadStream(pathpath);
+        stream.on("open", () => {
+            stream.pipe(res);
+        });
+
+        stream.on("error", () => {
+            res.statusCode = 404;
+            console.log("ERR|UNK: HSRV 404");
+            res.end("Not Found");
+        });
+        
+    } else {
+        // Test if this should be a redirect endpoint
+        let id;
+        for (var i in servers) {
+            if (parseIt.pathname == ("/" + servers[i].alias)) {
+                id = i;
+                break;
+            }
+        }
+
+        if (id) { // If it is
+            res.setHeader("Location", "http://" + req.headers.host + ":" + servers[id].port + "/");
+            res.writeHead(301, http.STATUS_CODES[301]);
+            res.end();
+        } else { // If it isn't
+            res.writeHead(404, http.STATUS_CODES[404]);
+            res.end();
+        }
+    }
+};
+
+Requests.listServers = function(_req, res, _data) {
+    res.end(JSON.stringify(Manager.listServers()));
+};
+
+Requests.getServer = function(_req, res, data) {
+    let body = JSON.parse(data.toString("utf8"));
+    res.end(JSON.stringify(Manager.getServer(body.id)));
+}
+
+Requests.newServer = function(_req, res, data) {
+    let body = JSON.parse(data.toString("utf8"));
+    let resss = Manager.newServer(body);
+
+    if (resss) {
+        res.end(JSON.stringify(resss));
+    }
+}
+
+
+
+
+
+
+
+
+
+let requestHandler = async function(req, res) {
+    let parseIt = url.parse(req.url, true);
+
+    let type = "400";
 
     if (req.method == "GET" && !parseIt.search) {
-        // This handles sending the pages required.
-        let pathpath = __dirname + "/remote" + parseIt.pathname + needsIndex;
+        type = "page";
+        Requests.path(req, res);
+    } else if (parseIt.query !== null && typeof parseIt.query == "object" && Object.keys(parseIt.query).length > 0) {
+        // This is a server method
+        type = "501";
+        callback;
+        let method = parseIt.query.method;
 
-        if (fsSync.existsSync(pathpath)) {
-            if (pathed.ext == ".html") {
-                res.setHeader("Content-Type", "text/html");
-            } else if (pathed.ext == ".js") {
-                res.setHeader("Content-Type", "application/js");
-            } else if (pathed.ext == ".css") {
-                res.setHeader("Content-Type", "text/css");
-            } else if (pathed.ext == ".png") {
-                res.setHeader("Content-Type", "image/png");
-            } else if (pathed.ext == ".svg") {
-                res.setHeader("Content-Type", "image/svg+xml");
-            } else if (pathed.ext == ".ttf") {
-                res.setHeader("Content-Type", "application/font");
+        if (req.method == "GET") { 
+            // Get requests only get information
+            if (method == "list") {
+                type = "list";
+                callback = Requests.listServers;
+            } else if (method == "getserver") {
+                type = "getserver";
+                callback = Requests.getServer;
             }
-
-            let stream = fsSync.createReadStream(pathpath);
-            stream.on("open", () => {
-                stream.pipe(res);
-            });
-
-            stream.on("error", () => {
-                res.statusCode = 404;
-                console.log("ERR|UNK: HSRV 404");
-                res.end("Not Found");
-            });
-            
-        } else {
-            let id;
-            for (var i in servers) {
-                if (parseIt.pathname == ("/" + servers[i].alias)) {
-                    id = i;
-                    break;
-                }
-            }
-
-            if (id) {
-                res.setHeader("Location", "http://" + req.headers.host + ":" + servers[id].port + "/");
-                res.writeHead(301, http.STATUS_CODES[301]);
-                res.end();
-            } else {
-                res.writeHead(404, http.STATUS_CODES[404]);
-                res.end();
+        } else if (req.method == "POST") {
+            // This one sends information
+            if (method == "newserver") {
+                type = "newserver";
+                callback = Requests.newServer;
             }
         }
-    } else if (req.method == "GET" && parseIt.query !== null && typeof parseIt.query == "object" && Object.keys(parseIt.query).length > 0) {
-        // Handle sending the required data for the resources and stuff
-        var method = parseIt.query.method;
-        if (method == "list") {
-            res.end(JSON.stringify(listServers()));
-        } else if (method == "getserver") {
-            res.end(JSON.stringify(getServer(parseIt.query.id)));
-        } else {
-            res.writeHead(501, http.STATUS_CODES[501]);
-            res.end();
-            console.error("ERR|UNK: GET REQ");
-        }
-    } else if (req.method == "POST" && parseIt.query !== null && typeof parseIt.query == "object" && Object.keys(parseIt.query).length > 0) {
-        // Handle receiving the face of the cheese burgers.
-        var method = parseIt.query.method;
-        if (method == "newserver") {
-            let resss = newServer({
-                id: parse.query.id,
-                alias: parse.query.id,
-                port: parse.query.id,
-                id: parse.query.id,
+
+        if (type !== "501") {
+            let buffer = Buffer.from([]);
+            req.on("data", (chunk) => {
+                buffer.concat(buffer, chunk);
             });
-
-            if (resss) {
-                res.end(JSON.stringify(resss));
-            }
-        } else {
-            res.writeHead(501, http.STATUS_CODES[501]);
-            console.log("ERR|UNK: POST REQ");
-            res.end();
+            req.on("end", () => {
+                callback(req, res, buffer);
+            });
         }
-
-    } else {
+    } 
+    
+    if (type == "400") {
         res.writeHead(400, http.STATUS_CODES[400]);
         console.log("ERR|UNK: MALFORMED");
         res.end();
     }
+
+    if (type == "501") {
+        res.writeHead(501, http.STATUS_CODES[501]);
+        console.log("ERR|UNK: IDK REQ");
+        res.end();
+    }
 };
 
-let serverboi = http.createServer(handleAllRequests);
+let serverboi = http.createServer(requestHandler);
 
 serverboi.listen({
     port: networkingPort
--- remote/main.js
+++ remote/main.js
@@ -12,6 +12,17 @@ Home.servers.list = async function() {
     });
 };
 
+Home.servers.get = async function(id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let servers = JSON.parse(await getRequest("getserver", {id: id}));
+            resolve(servers);
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
 let UI = {};
 UI.Presets = {};
 
@@ -30,22 +41,45 @@ UI.Presets.ButtonToggle = function(button) {
 
 UI.Presets.ButtonToggle(find("button.button.toggle"));
 
+UI.showEdit = async function(id) {
+    document.body.attr("data-mode", "edit");
+
+    // Do something
+
+    console.log(await Home.servers.get(id))
+};
+
+UI.showConfig = async function() {
+    try {
+        let ssvvrr = await Home.servers.list();
+        find("div.configcanvas").innerHTML = "";
+        for (let i in ssvvrr) {
+            UI.Components.serverLine(ssvvrr[i]);
+        }
+        document.body.attr("data-mode", "config");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
 UI.Components = {};
 
 UI.Components.serverLine = function(payload) {
     let root = document.createElement("div").attr("data-id", payload.id).chng("className", "server-line cont");
     
-    let idEL = document.createElement("div").chng("className", "button server-line id").chng("innerText", payload.id);
+    let idEL = document.createElement("div").chng("className", "server-line id").chng("innerText", payload.id);
     let aliasEL = document.createElement("div").chng("className", "server-line alias").chng("type", "text").chng("innerText", payload.alias);
     let portEL = document.createElement("div").chng("className", "server-line port").chng("type", "text").chng("innerText", payload.port);
     let runonbootEL = document.createElement("button").chng("className", "button toggle server-line runonboot").chng("disabled", true).chng("innerText", "Run On Boot");
     let runfileEL = document.createElement("div").chng("className", "server-line runfile").chng("type", "text").chng("innerText", payload.runfile);
-    let updateEL = document.createElement("button").chng("className", "button server-line update").chng("innerText", "Edit").attr("data-id", payload.id);
+    let editEL = document.createElement("button").chng("className", "button server-line edit").chng("innerText", "Edit").attr("data-id", payload.id);
     if (payload.runonboot) {
         runonbootEL.attr("data-checked", true);
     }
+    editEL.when("click", () => {UI.showEdit(payload.id)});
 
-    root.append(idEL, aliasEL, portEL, runonbootEL, runfileEL, updateEL);
+    root.append(idEL, aliasEL, portEL, runonbootEL, runfileEL, editEL);
 
     find("div.configcanvas").append(root);
 };
@@ -54,15 +88,6 @@ UI.Components.serverLine = function(payload) {
 
 
 
-let setup = async function() {
-    try {
-        let ssvvrr = await Home.servers.list();
-        for (var i in ssvvrr) {
-            UI.Components.serverLine(ssvvrr[i]);
-        }
-    } catch (err) {
-        console.log(err);
-    }
-};
 
-setup();
+
+UI.showConfig();
